393ff3b7476084b390e2253585fabfe1364b20a2
Mark Schmieder <markhschmieder@gmail.com>
2026-01-24 17:19:13 -0500

    Update all copyrights to 2026 and make consistent.

--- src/main/java/org/jfxconverter/drivers/ppt/PPTGraphics2D.java
+++ src/main/java/org/jfxconverter/drivers/ppt/PPTGraphics2D.java
@@ -1,7 +1,7 @@
 /**
  * Copyright (c) 2011, 2013, 2014, 2015, 2016 Dassault Aviation.
  * Copyright (c) 2017 Herve Girod.
- * All rights reserved.
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
  * 1. Redistributions of source code must retain the above copyright notice,
